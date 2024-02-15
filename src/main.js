fd8a7bad02dce50e515278ddad75e3861c9b6ad4
barbara <barbara.mascena@gmail.com>
2024-02-15 11:31:36 -0300

    Estrutura básica html com filtros e botão.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import { example } from './dataFunctions.js';
+//import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
+const listaCard = document.querySelector('#root');
 
-console.log(example, renderItems(data), data);
+document.addEventListener("DOMContentLoaded", () => {
+  listaCard.appendChild(renderItems(data))
+})
+
+//document.querySelector("#root").innerHTML = renderItems(data)
+
+//console.log(example, renderItems(data), data);
+//const root = document.querySelector('#root ul') // puxando a tag ul atraves do elemento id
+
+//for(let serie of data){
+//   root.innerHTML = root.innerHTML + renderItems(serie)
+//}
+
+//root.innerHTML = root.innerHTML + renderItems(data[0])
--- src/view.js
+++ src/view.js
@@ -1,6 +1,20 @@
 export const renderItems = (data) => {
-  console.log(data)
-  // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+
+  const doramacard = document.createElement('ul');
+  doramacard.classList.add('container-doramacard');
+
+  data.forEach(itemData => {
+    doramacard.innerHTML += `
+            <li itemscope itemtype="https://www.imdb.com/list/ls086959587/" class="container-itemdata">
+                <dl itemscope itemtype="https://www.imdb.com/list/ls086959587/">
+                    <dt><img src="${itemData.imageUrl}" alt="Imagem do card" itemprop="imageUrl" class="imagem_card" /></dt>
+                    <dt>Nome:</dt><dd itemprop="name">${itemData.name}</dd>
+                    <dt>Descrição:</dt><dd itemprop="description">${itemData.shortDescription}</dd>
+                    <dt>Sinopse:</dt><dd itemprop="description">${itemData.description}</dd>
+                    <dt>Fatos:</dt><dd itemprop="facts">${itemData.facts.doramaRating}</dd>
+                 </dl>
+            </li>`;
+  })
+  return doramacard;
 };
 
