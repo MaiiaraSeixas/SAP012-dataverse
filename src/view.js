769cf4101c15d5064a50261788555fb8258ec481
barbara <barbara.mascena@gmail.com>
2024-02-16 21:04:59 -0300

    viwe atualizado, com alterações na estrutura para se adaptar ao CSS

--- src/view.js
+++ src/view.js
@@ -5,13 +5,14 @@ export const renderItems = (data) => {
 
   data.forEach(itemData => {
     doramacard.innerHTML += `
-            <li itemscope itemtype="https://www.imdb.com/list/ls086959587/" class="container-itemdata">
+            <li itemscope itemtype="https://www.imdb.com/list/ls086959587/" class="container_li">
                 <dl itemscope itemtype="https://www.imdb.com/list/ls086959587/">
-                    <dt><img src="${itemData.imageUrl}" alt="Imagem do card" itemprop="imageUrl" class="imagem_card" /></dt>
-                    <dt>Nome:</dt><dd itemprop="name">${itemData.name}</dd>
-                    <dt>Descrição:</dt><dd itemprop="description">${itemData.shortDescription}</dd>
-                    <dt>Sinopse:</dt><dd itemprop="description">${itemData.description}</dd>
-                    <dt>Fatos:</dt><dd itemprop="facts">${itemData.facts.doramaRating}</dd>
+                    <dt><img src="${itemData.imageUrl}" alt="Imagem do card" itemprop="imageUrl" class="img_card" /></dt>
+                    <dt>${itemData.name}</dt>
+                    <dt>Avaliação:</dt>
+                      <dd itemprop="facts">${itemData.facts.doramaRating}</dd>
+                    <dt>Descrição:</dt>
+                      <dd itemprop="description">${itemData.shortDescription}</dd>
                  </dl>
             </li>`;
   })
