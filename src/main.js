ee7b6d3a8eb7a7f45962d0f77466f11b5a46b373
barbara <barbara.mascena@gmail.com>
2024-02-16 21:00:45 -0300

    HTML atualizado liberado pelos testes

--- src/main.js
+++ src/main.js
@@ -3,9 +3,21 @@ import { renderItems } from './view.js';
 
 import data from './data/dataset.js';
 const listaCard = document.querySelector('#root');
+const filtros = document.getElementById('filtros');
+const ordenacao = document.getElementById('ordenacao');
+const botao = document.querySelector('[data-testid="button-clear"]')
 
 document.addEventListener("DOMContentLoaded", () => {
-  listaCard.appendChild(renderItems(data))
+  listaCard.appendChild(renderItems(data));
+  filtros.addEventListener('change', () =>{
+    console.log ('vai filtrar');
+  });
+  ordenacao.addEventListener('change', () =>{
+    console.log (' vai ordenar');
+  });
+  botao.addEventListener('click', () =>{
+    console.log (' vai limpar');
+  });
 })
 
 //document.querySelector("#root").innerHTML = renderItems(data)
