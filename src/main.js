6503c5d6842b4590e5b165db526b7b31bc98b205
barbara <barbara.mascena@gmail.com>
2024-02-29 16:27:10 -0300

    atualização com as informações da estatística

--- src/main.js
+++ src/main.js
@@ -1,28 +1,43 @@
 import { renderItems } from './view.js';
 import data from './data/dataset.js';
-import { filterdata, orderdata } from './dataFunctions.js'; 
+import { filterdata, orderdata, computStats} from './dataFunctions.js'; 
+
 const listaCard = document.querySelector('#root');
 const filter = document.querySelector('#filter');
 const order = document.querySelector('#order');
 const button = document.querySelector('[data-testid="button_clear"]')
+const calculo_estatistica = document.querySelector('.calculo_estatistica');
+
 let dadosExibidos = data.slice();
+
+const totalcards = (data) => {
+  const numbercards = computStats(data);
+  calculo_estatistica.innerHTML=`${numbercards} cards`;
+};
 
 document.addEventListener("DOMContentLoaded", () => {
   listaCard.appendChild(renderItems(dadosExibidos));
+  totalcards(data);
 
   filter.addEventListener('change', (event) => {
-    dadosExibidos = filterdata(dadosExibidos, event.target.value);
+    dadosExibidos = filterdata(data, event.target.value);
     listaCard.innerHTML = ""
     listaCard.appendChild(renderItems(dadosExibidos))
+    totalcards(dadosExibidos);
   });
+
   order.addEventListener('change', (event) => {
     dadosExibidos = orderdata(dadosExibidos, event.target.value);
     listaCard.innerHTML = "";
     listaCard.appendChild(renderItems(dadosExibidos));
+    totalcards(dadosExibidos);
+    
   });
   button.addEventListener('click', (event) => {
     listaCard.innerHTML = "";
     dadosExibidos = data.slice();
     listaCard.appendChild(renderItems(data));
+    totalcards(dadosExibidos);
   });
 });
+
